bb5dba28fa271706b6f59130cfc8a2e21cfbc23d
André Costa <74073671+Andre-N-Costa@users.noreply.github.com>
2022-02-28 14:57:45 +0000

    Update exercicio4.c

--- guiao1/exercicio4.c
+++ guiao1/exercicio4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <sys/types.h>
-#include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
-#include <fcntl.h> /* O_RDONLY, O_WRONLY, O_CREAT, O_* */
+#include <unistd.h>
+#include <fcntl.h>
 
 ssize_t readln(int fd, char *line, size_t size) {
  int n;
