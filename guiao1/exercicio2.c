9b6b68e1316cf16ec0bf781c134567a4d205d343
André Costa <74073671+Andre-N-Costa@users.noreply.github.com>
2022-02-28 14:56:16 +0000

    Create exercicio2.c

---
+++ guiao1/exercicio2.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+#define MAX_BUFFER 1024
+
+int main(int argc, char *argv[]){
+  char buffer[MAX_BUFFER];
+  int bytes_read;
+
+  while((bytes_read = read(0, buffer, MAX_BUFFER)) > 0){
+    write(1,buffer,bytes_read);
+  }
+  close(bytes_read);
+  printf("terminei\n");
+
+  return 0;
+}
