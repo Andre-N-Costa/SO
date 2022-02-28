bbff09af7e90cc7d0ff0b94c86e9c16d3a92134e
André Costa <74073671+Andre-N-Costa@users.noreply.github.com>
2022-02-28 14:57:33 +0000

    Create exercicio4.c

---
+++ guiao1/exercicio4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
+#include <fcntl.h> /* O_RDONLY, O_WRONLY, O_CREAT, O_* */
+
+ssize_t readln(int fd, char *line, size_t size) {
+ int n;
+ char c;
+ ssize_t nbytes = 0;
+ while (c != '\n' && nbytes < size && read(fd,&c,1) > 0){
+    line[nbytes] = c;
+    nbytes++;
+ }
+ return nbytes;
+}
+
+int main(int argc, char* argv[]){
+  if (argc > 2) {
+      fprintf(stderr, "ERRO : demasiados argumentos");
+      return 1;
+  }
+  int fd = 0;
+  if (argc == 2) {
+      fd = open(argv[1], O_RDONLY);
+  }
+  if (fd == -1) {
+      perror(argv[1]);
+      return 1;
+  }
+  char buffer[1024], n_buffer[32];
+  int bytes, counter = 1;
+  while ((bytes = readln(fd, buffer, 1024)) > 0) {
+      sprintf(n_buffer, "%d  ", counter);
+      write(1, n_buffer, 2);
+      write(1, buffer, bytes);
+      write(1, "\n", 1);
+      counter++;
+  }
+  return 0;
+}
